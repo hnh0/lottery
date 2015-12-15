4bb36e2184ada1eea73dc8d341b3fa9917ed36f9
Kangning <liu.kangning@163.com>
2015-12-16 00:47:53 +0800

    homepage

--- js/main.js
+++ js/main.js
@@ -1,6 +1,37 @@
-var lottey = window.lottey || {};
+var lottery = window.lottery || {};
 (function () {
-	lottey = {
-
+	lottery = {
+		btn: $('#btn'),
+		gif: $('#gif'),
+		interval: {},
+		init: function(){
+			var self = this
+			self.bind()
+		},
+		bind: function(){
+			var self = this
+			self.interval = setInterval('lottery.loop()', 500)
+			self.btn.click(function(event) {
+				self.play()
+			});
+		},
+		loop: function(){
+			var self = this	
+			self.btn.toggleClass('down')
+		},
+		play: function(){
+			var self = this
+			self.gif.addClass('show')
+			setTimeout('lottery.go()', 3000)
+		},
+		go: function(){
+			var self = this,
+			sUrl = self.btn.data('url')
+			window.location.href = sUrl
+		}
 	}
+
+	$(function(){
+		lottery.init()
+	})
 })()
